73525a402da1174f92461be4099b646f018745e7
sisocobacho <110941609+sisocobacho@users.noreply.github.com>
2023-01-18 10:11:32 -0500

    primer ejercicio

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Cuban Real State",
+    'summary': 'Cuban real state management',
+    'description': 'Cuban real state management',
+    'version' : '0.1',
+    'category': 'Sales',
+    'author': "Ocobacho",
+    'depends': [
+        'base_setup',
+    ],
+}
