0232b12d43be3d62147f1c272cae47e5199919ee
sisocobacho <110941609+sisocobacho@users.noreply.github.com>
2023-01-19 12:10:43 -0500

    En clase 19 enero

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_tags_views.xml',
         'views/estate_menu.xml',
-        
     ]
 }
--- models/estate_property.py
+++ models/estate_property.py
@@ -7,6 +7,7 @@ class EstateProperty(models.Model):
     _order = 'name ASC'
 
     name = fields.Char(required=True)
+    tags = fields.Many2many('estate.property.tag')
     description = fields.Text()
     postcode = fields.Text()
     date_availability = fields.Date()
@@ -17,8 +18,43 @@ class EstateProperty(models.Model):
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
+    property_type_id = fields.Many2one("estate.property.type", string="Poperty Type")
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'),
                    ('east', 'East'), ('west', 'West'),],
     )
+
+
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = 'PropertyType'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+    real_estate = fields.One2many("estate.property", "property_type_id", string="Real Estates")
+
+
+
+
+class EstatePropertyTag(models.Model):
+    _name = 'estate.property.tag'
+    _description = 'PropertyTag'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+
+
+    
+ 
