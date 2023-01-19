e40d3090d3174b80ef343e958c20455196a198a1
Niorlys <Niorlys>
2023-01-19 00:22:36 -0500

    Addon modules created

---
+++ addons/Library/__manifest__.py
@@ -0,0 +1,15 @@
+{'name':'My library',
+'summary': "Manage books easily",
+'description': """
+Manage Module
+==============
+Description related to My module.
+""",
+'author': "Niorlys",
+'website': "http://www.example.com",
+'category': 'Uncategorized',
+'version': '13.0.1',
+'depends': ['base'],
+'data': ['views/views.xml'],
+'demo': ['demo.xml'],
+}
---
+++ addons/ReA/__manifest__.py
@@ -0,0 +1,15 @@
+{'name':'Real State Advertisement',
+'summary': "Promote avaliable home for buying",
+'description': """
+Advertisement Module
+==============
+Promote your house sale proposition to the world in order to build a brand, attract clients, and close real estate transactions.
+""",
+'author': "Niorlys",
+'website': "",
+'category': 'Uncategorized',
+'version': '13.0.1',
+'depends': ['base'],
+'data': ['views/views.xml'],
+'demo': ['demo.xml'],
+}
