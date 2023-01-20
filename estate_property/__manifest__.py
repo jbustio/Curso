9c1660dc824349f1dd86cc7dea6c2fa70f7364d3
DiazRock <corolariodiaz@gmail.com>
2023-01-20 01:24:35 -0500

    Added a view for estate_properties

--- estate_property/__manifest__.py
+++ estate_property/__manifest__.py
@@ -26,7 +26,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        'views/templates.xml'
+        #'views/real_state.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
