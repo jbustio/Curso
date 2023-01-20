d588482c21fc788454d0fb645889a0de482fd511
sisocobacho <110941609+sisocobacho@users.noreply.github.com>
2023-01-20 00:42:00 -0500

    tarea clase 19/01/23

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
@@ -6,11 +7,13 @@ class EstateProperty(models.Model):
     _description = 'Real Estate Property'
     _order = 'name ASC'
 
-    name = fields.Char(required=True)
-    tags = fields.Many2many('estate.property.tag')
+    name = fields.Char(default=lambda self: 'New Real Estate', required=True)
+    tag_ids = fields.Many2many('estate.property.tag')
     description = fields.Text()
-    postcode = fields.Text()
-    date_availability = fields.Date()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        default=lambda self: fields.datetime.now() + relativedelta(months=2),
+        copy=False, string="Available from")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
@@ -18,13 +21,17 @@ class EstateProperty(models.Model):
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    property_type_id = fields.Many2one("estate.property.type", string="Poperty Type")
+    property_type_id = fields.Many2one(
+        "estate.property.type", string="Property Type")
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'),
                    ('east', 'East'), ('west', 'West'),],
     )
-
+    salesperson_id = fields.Many2one(
+        'res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
+    offer_ids = fields.One2many('estate.property.offer', 'property_id')
 
 
 class EstatePropertyType(models.Model):
@@ -38,9 +45,8 @@ class EstatePropertyType(models.Model):
         string='Name',
         required=True,
     )
-    real_estate = fields.One2many("estate.property", "property_type_id", string="Real Estates")
-
-
+    real_estate = fields.One2many(
+        "estate.property", "property_type_id", string="Real Estates")
 
 
 class EstatePropertyTag(models.Model):
@@ -56,5 +62,16 @@ class EstatePropertyTag(models.Model):
     )
 
 
-    
- 
+class EstatePropertyOffer(models.Model):
+    _name = 'estate.property.offer'
+    _description = 'EstatePropertyOffer'
+
+    price = fields.Float()
+    status = fields.Selection(
+        selection=[('accepted', 'Accepted'), ('refuse', 'Refuse'),], copy=False)
+    partner_id = fields.Many2one('res.partner', string="Partner",
+                                 required=True
+                                 )
+    property_id = fields.Many2one('estate.property',
+                                  required=True
+                                  )
