4f32faf942d3415cfa4a56e76dd8284bae5cdf6d
DiazRock <corolariodiaz@gmail.com>
2023-01-20 01:24:47 -0500

    Added a model

--- estate_property/models/models.py
+++ estate_property/models/models.py
@@ -7,10 +7,25 @@ class estate_property(models.Model):
     _name = 'estate_property.estate_property'
     _description = 'estate_property.estate_property'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
+    name = fields.Char(required = True)
     description = fields.Text()
+    postcode = fields.Char(compute="_value_pc", store=True)
+    date_availabity = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden area')
+    garden_orientation = fields.Selection([
+        ('North', 'North'),
+        ('South', 'South'),
+        ('East', 'East'),
+        ('West', 'West'),
+    ], string='Garden orientation')
+
 
     @api.depends('value')
     def _value_pc(self):
