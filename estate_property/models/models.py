877cea1ce2764ee9afcb434a06a355fa82eda831
DiazRock <corolariodiaz@gmail.com>
2023-01-20 14:57:52 -0500

    Added new fields

--- estate_property/models/models.py
+++ estate_property/models/models.py
@@ -11,9 +11,9 @@ class estate_property(models.Model):
     name = fields.Char(required = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy = False)
     expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
+    selling_price = fields.Float(readonly = True)
     bedrooms = fields.Integer()
     living_area = fields.Integer()
     facades = fields.Integer('Facades')
@@ -26,7 +26,14 @@ class estate_property(models.Model):
         ('East', 'East'),
         ('West', 'West'),
     ], string='Garden orientation')
-
+    active = fields.Boolean(default = True)
+    state = fields.Selection([
+        ('New', 'New'),
+        ('Offer Received', 'Offer Received'), 
+        ('Offer Accepted', 'Offer Accepted'), 
+        ('Sold', 'Sold'),
+        ('Canceled', 'Canceled')
+    ])
 
     @api.depends('value')
     def _value_pc(self):
