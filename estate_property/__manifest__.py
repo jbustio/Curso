9c7e022a554df49254698426e24272f088502bde
DiazRock <corolariodiaz@gmail.com>
2023-01-20 02:28:35 -0500

    Actualized views reference

--- estate_property/__manifest__.py
+++ estate_property/__manifest__.py
@@ -27,7 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml'
-        #'views/real_state.xml'
+        #'views/real_state_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
