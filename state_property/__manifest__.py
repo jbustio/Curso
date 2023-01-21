631d9c87d81ac39f67a8a8f3c54886e427019cc6
Forbidden <lloduardo@gmail.com>
2023-01-20 23:00:48 -0500

    Add() - Add file "menu_views.xml" to show the module menu.

--- state_property/__manifest__.py
+++ state_property/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/menu_views.xml',
         'views/state_property.xml',
     ],
     # only loaded in demonstration mode
