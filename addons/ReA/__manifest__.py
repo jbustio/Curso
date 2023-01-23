bccbed8c79862d85c8e3d265d088dbd1c62cd9d3
Niorlys <Niorlys>
2023-01-23 01:03:46 -0500

    Added security access

--- addons/ReA/__manifest__.py
+++ addons/ReA/__manifest__.py
@@ -1,14 +1,21 @@
-{'name':'Real State Advertisement',
+{
+'name':'Real State Advertisement',
 'summary': "Promote avaliable home for buying",
 'description': """
 Advertisement Module
-==============
+====================
 Promote your house sale proposition to the world in order to build a brand, attract clients, and close real estate transactions.
 """,
 'author': "Niorlys",
 'website': "",
-'category': 'Uncategorized',
+'category': 'Sales/Property',
+'depends': ['base'],
 'version': '13.0.1',
-'data': ['views/estate_property_views.xml'
-]
+'data': [
+    'security/rea_security_groups.xml',
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml'
+],
+"license": "AGPL-3",
+"application": True,
 }
