01853249b4691081d402c38953c7323298648f54
Jesús Cobacho Aguilera <jcobacho90@gmail.com>
2023-01-26 11:29:53 -0500

    created invoice_id relation with real estate property and associate it on sold action

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -25,11 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'reports/real_estate_property_template.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,8 +1,13 @@
+import datetime
+
 from odoo import models, Command
+from odoo import models, fields, api
 
 
 class RealEstatePropertyAccountModel(models.Model):
     _inherit = "real.estate.property"
+
+    invoice_id = fields.Many2one('account.move', string="Invoice")
 
     def _calc_selling_price_percent(self, selling_price):
         return selling_price * 0.06
@@ -10,16 +15,18 @@ class RealEstatePropertyAccountModel(models.Model):
     def sell_property(self):
         invoice_vals = {
 
+            'name': f'realestate/{self.property_type_id.name}/{self.id}',
             'move_type': 'out_invoice',
             'partner_id': self.buyer_id.id,
+            'invoice_date': datetime.datetime.today(),
             "line_ids": [
-                Command.create({
+                (0, 0, {
                     "name": f"{self.name} (6%)",
                     "quantity": "1",
                     "price_unit": self._calc_selling_price_percent(self.selling_price),
                     "tax_ids": False
                 }),
-                Command.create({
+                (0, 0, {
                     "name": "administrative fees",
                     "quantity": "1",
                     "price_unit": 100.00,
@@ -29,5 +36,5 @@ class RealEstatePropertyAccountModel(models.Model):
             ],
 
         }
-        self.env['account.move'].create(invoice_vals)
+        self.invoice_id = self.env['account.move'].create(invoice_vals)
         return super().sell_property()
