0e300afde50967a47ba8b2ddf39dec2cd97c5b49
DiazRock <corolariodiaz@gmail.com>
2023-01-26 22:39:31 -0500

    Manifest

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'report/estate_report.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
