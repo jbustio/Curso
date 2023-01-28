b27121ee32fbde34a0c2ef48a21d58574dd21e12
Niorlys <Niorlys>
2023-01-28 13:53:59 -0500

    Exercise 28

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -4,5 +4,6 @@ class EstateProperty(models.Model):
     _inherit="estate.property"
 
     def set_property_as_sold(self):
-        print('IT IS WORKING!!!')
+        print('INSIDE')
+        move = self.env['account.move'].create({'partner_id':self.buyer_id, 'move_type':'out_invoice'})
         return super().set_property_as_sold()
