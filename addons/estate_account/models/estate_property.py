e9c8f211bec21d5a99f7d760c08bee0de906dd2f
Niorlys <Niorlys>
2023-01-28 19:44:58 -0500

    Exercise 29

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -1,9 +1,12 @@
-from odoo import models
+from odoo import models, Command
 
 class EstateProperty(models.Model):
     _inherit="estate.property"
 
     def set_property_as_sold(self):
         print('INSIDE')
-        move = self.env['account.move'].create({'partner_id':self.buyer_id, 'move_type':'out_invoice'})
+        move = self.env['account.move'].create({'partner_id':self.buyer_id, 'move_type':'out_invoice',
+                                                    'invoice_line_ids':[Command.create({'name':'Tax', 
+                                                    'quantity':self.selling_price * 6 /100, 'price_unit':self.selling_price})
+                                                    ,Command.create({'name':'Administrative Fees', 'quantity':100, 'price_unit':0})]})
         return super().set_property_as_sold()
