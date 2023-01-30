58661cd386c933c21a4d67c9c610d0fdbba70388
DiazRock <corolariodiaz@gmail.com>
2023-01-30 02:01:43 -0500

    Some constraints added

--- applicants/models/applicant.py
+++ applicants/models/applicant.py
@@ -9,28 +9,34 @@ class applicants(models.Model):
     _description = 'applicants.applicants'
 
     name = fields.Char(required = True, string = 'Name')
-    lastName1 = fields.Char(string = 'Last Name1')
-    lastName2 = fields.Char(string="Last Name2")
-    CI = fields.Char(required=True, string="CI")
+    last_name1 = fields.Char(string = 'Last Name1')
+    last_name2 = fields.Char(string="Last Name2")
+    ci = fields.Char(required=True, string="CI")
     address = fields.Char(required=True, string="address")
-    Age = fields.Integer(required=True, string="Age")
-    Sex = fields.Selection([
+    age = fields.Integer(required=True, string="Age", default=18)
+    sex = fields.Selection([
         ('Female', 'Female'),
         ('Male', 'Male')
     ], string='Sex')
-    company_id = fields.Many2many('res.partner', default = 'Company')
+    company_id = fields.Many2many('res.partner', string='Company')
     technology_and_experience_id = fields.One2many(
         'applicants.technology_and_experience',
         'applicant_id',
         string="Technology and experience"
         )
 
-    
+    _sql_constraints = [
+        (
+            'check_age_lgq_18', 
+            'CHECK(age >= 18)',
+            'The applicant must be in legal age'
+        )
+    ]
 
-    @api.constrains('CI')
-    def _check_CI(self):
+    @api.constrains('ci')
+    def _check_ci(self):
         reg = re.compile("\d{11}")
         for record in self:
-            if not reg.fullmatch(record.CI):
+            if not reg.fullmatch(record.ci):
                 raise ValidationError("The CI must be a number of eleven digits")
 
